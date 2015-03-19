7261460117bdcd1d24499128f4db72039e16275d
Nuno Gomes <nuno_andre_7@hotmail.com>
2015-03-19 22:55:47 +0000

    Slogan partial added

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
 
     resources :articles
-    root 'welcome#index'
+    root 'articles#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
