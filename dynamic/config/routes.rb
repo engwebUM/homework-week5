d6946a73b7f94a3fad9f0b0aebc85fcfebc56001
guterres <guterres25@gmail.com>
2015-03-22 00:15:03 +0000

    modified file routes

--- dynamic/config/routes.rb
+++ dynamic/config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  resources :articles
-
-  resources :icons
-
-  resources :authors
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -59,7 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
- 
+
   root to: "articles#index"
-  resources :articles
+  resources :articles, only: [:index]
 end
