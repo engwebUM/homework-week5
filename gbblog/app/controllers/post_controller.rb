2af38b04e17132811a91ec7abbdb4fed7088d9ab
nunomsantos <nuno21nt@gmail.com>
2015-03-22 22:33:30 +0000

    Dynamically generating page title when viewing a single post

--- gbblog/app/controllers/post_controller.rb
+++ gbblog/app/controllers/post_controller.rb
@@ -5,6 +5,7 @@ class PostController < ApplicationController
 
   def show
     @posts = [Post.find(params[:id])]
+    @page_title = "#{@posts[0].title}, by #{@posts[0].author.name} of Group Buddies"
     render 'index'
   end
 end
