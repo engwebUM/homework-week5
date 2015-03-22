5ea2df3907a1f60c23de615d83148e64b5ebe08a
nunomsantos <nuno21nt@gmail.com>
2015-03-22 19:24:04 +0000

    Twitter share button is now working

--- gbblog/db/migrate/20150322075548_add_author_to_posts.rb
+++ gbblog/db/migrate/20150322075548_add_author_to_posts.rb
@@ -1,9 +1,5 @@
 class AddAuthorToPosts < ActiveRecord::Migration
   def change
     add_reference :posts, :author, index:true, foreign_key: true
-    # add_index :posts, :author
-    # add_foreign_key :posts, :author
-    #
-    # add_reference :uploads, :user, index: true
   end
 end
