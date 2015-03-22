28cb1cb9a59575c707cd9273170b4cc5f0341403
nunomsantos <nuno21nt@gmail.com>
2015-03-22 18:34:06 +0000

    Added style to hovered links; fixed a bug on loading archives from João; added seeds to populate database

--- gbblog/app/controllers/category_controller.rb
+++ gbblog/app/controllers/category_controller.rb
@@ -1,6 +1,7 @@
 class CategoryController < ApplicationController
   def show
-    @posts = Post.where(category: Category.where(name: params[:id])).order("created_at desc")
+    @posts = Post.where(category: Category.where(name: params[:id].titleize)).order("created_at desc")
+    # @posts = Post.where(category: Category.where(id: params[:id])).order("created_at desc")
 
     render 'archive/index'
   end
